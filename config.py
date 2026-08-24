2da3d15992d4b41be65ebc2f8bce6554be97dba1
yeferson <siryefers6@gmail.com>
2026-08-23 21:06:07 -0400

    test: crear medidor con 2 camaras

---
+++ config.py
@@ -0,0 +1,176 @@
+from classes.grupo_arucos_lineal import GrupoArucosLineal
+
+arucos_ancho = [
+    GrupoArucosLineal(
+        id_inicial=0,
+        id_final=19,
+        milimetros_pared_a_primer_aruco=20,
+        tamanio_aruco_milimetros=20,
+        separacion_arucos_en_milimetros=5,
+        esquina_referencia_medicion=0,
+    ),
+    GrupoArucosLineal(
+        id_inicial=20,
+        id_final=39,
+        milimetros_pared_a_primer_aruco=25,
+        tamanio_aruco_milimetros=20,
+        separacion_arucos_en_milimetros=5,
+        esquina_referencia_medicion=0,
+    ),
+    GrupoArucosLineal(
+        id_inicial=40,
+        id_final=59,
+        milimetros_pared_a_primer_aruco=30,
+        tamanio_aruco_milimetros=20,
+        separacion_arucos_en_milimetros=5,
+        esquina_referencia_medicion=0,
+    ),
+    GrupoArucosLineal(
+        id_inicial=60,
+        id_final=79,
+        milimetros_pared_a_primer_aruco=35,
+        tamanio_aruco_milimetros=20,
+        separacion_arucos_en_milimetros=5,
+        esquina_referencia_medicion=0,
+    ),
+    GrupoArucosLineal(
+        id_inicial=80,
+        id_final=99,
+        milimetros_pared_a_primer_aruco=40,
+        tamanio_aruco_milimetros=20,
+        separacion_arucos_en_milimetros=5,
+        esquina_referencia_medicion=0,
+    ),
+]
+
+arucos_largo = [
+    GrupoArucosLineal(
+        id_inicial=100,
+        id_final=119,
+        milimetros_pared_a_primer_aruco=20,
+        tamanio_aruco_milimetros=20,
+        separacion_arucos_en_milimetros=5,
+        esquina_referencia_medicion=0,
+    ),
+    GrupoArucosLineal(
+        id_inicial=120,
+        id_final=139,
+        milimetros_pared_a_primer_aruco=25,
+        tamanio_aruco_milimetros=20,
+        separacion_arucos_en_milimetros=5,
+        esquina_referencia_medicion=0,
+    ),
+    GrupoArucosLineal(
+        id_inicial=140,
+        id_final=159,
+        milimetros_pared_a_primer_aruco=30,
+        tamanio_aruco_milimetros=20,
+        separacion_arucos_en_milimetros=5,
+        esquina_referencia_medicion=0,
+    ),
+    GrupoArucosLineal(
+        id_inicial=160,
+        id_final=179,
+        milimetros_pared_a_primer_aruco=35,
+        tamanio_aruco_milimetros=20,
+        separacion_arucos_en_milimetros=5,
+        esquina_referencia_medicion=0,
+    ),
+    GrupoArucosLineal(
+        id_inicial=180,
+        id_final=199,
+        milimetros_pared_a_primer_aruco=40,
+        tamanio_aruco_milimetros=20,
+        separacion_arucos_en_milimetros=5,
+        esquina_referencia_medicion=0,
+    ),
+]
+
+# # Grupo de ArUcos camera_4
+# arucos_camara_4_fila_1 = GrupoArucosLineal(
+#     id_inicial=200,
+#     id_final=219,
+#     milimetros_pared_a_primer_aruco=20,
+#     tamanio_aruco_milimetros=20,
+#     separacion_arucos_en_milimetros=5,
+#     esquina_referencia_medicion=0,
+# )
+
+# arucos_camara_4_fila_2 = GrupoArucosLineal(
+#     id_inicial=220,
+#     id_final=239,
+#     milimetros_pared_a_primer_aruco=25,
+#     tamanio_aruco_milimetros=20,
+#     separacion_arucos_en_milimetros=5,
+#     esquina_referencia_medicion=0,
+# )
+
+# arucos_camara_4_fila_3 = GrupoArucosLineal(
+#     id_inicial=240,
+#     id_final=259,
+#     milimetros_pared_a_primer_aruco=30,
+#     tamanio_aruco_milimetros=20,
+#     separacion_arucos_en_milimetros=5,
+#     esquina_referencia_medicion=0,
+# )
+
+# arucos_camara_4_fila_4 = GrupoArucosLineal(
+#     id_inicial=260,
+#     id_final=279,
+#     milimetros_pared_a_primer_aruco=35,
+#     tamanio_aruco_milimetros=20,
+#     separacion_arucos_en_milimetros=5,
+#     esquina_referencia_medicion=0,
+# )
+
+# arucos_camara_4_fila_5 = GrupoArucosLineal(
+#     id_inicial=280,
+#     id_final=299,
+#     milimetros_pared_a_primer_aruco=40,
+#     tamanio_aruco_milimetros=20,
+#     separacion_arucos_en_milimetros=5,
+#     esquina_referencia_medicion=0,
+# )
+
+arucos_alto = [
+    GrupoArucosLineal(
+        id_inicial=300,
+        id_final=319,
+        milimetros_pared_a_primer_aruco=20,
+        tamanio_aruco_milimetros=20,
+        separacion_arucos_en_milimetros=5,
+        esquina_referencia_medicion=0,
+    ),
+    GrupoArucosLineal(
+        id_inicial=320,
+        id_final=339,
+        milimetros_pared_a_primer_aruco=25,
+        tamanio_aruco_milimetros=20,
+        separacion_arucos_en_milimetros=5,
+        esquina_referencia_medicion=0,
+    ),
+    GrupoArucosLineal(
+        id_inicial=340,
+        id_final=359,
+        milimetros_pared_a_primer_aruco=30,
+        tamanio_aruco_milimetros=20,
+        separacion_arucos_en_milimetros=5,
+        esquina_referencia_medicion=0,
+    ),
+    GrupoArucosLineal(
+        id_inicial=360,
+        id_final=379,
+        milimetros_pared_a_primer_aruco=35,
+        tamanio_aruco_milimetros=20,
+        separacion_arucos_en_milimetros=5,
+        esquina_referencia_medicion=0,
+    ),
+    GrupoArucosLineal(
+        id_inicial=380,
+        id_final=399,
+        milimetros_pared_a_primer_aruco=40,
+        tamanio_aruco_milimetros=20,
+        separacion_arucos_en_milimetros=5,
+        esquina_referencia_medicion=0,
+    ),
+]
--- testing.py
+++ testing.py
@@ -1,13 +1,23 @@
 import time
+from statistics import mode
 
 import cv2
 
+import config
 from classes.camera_thread import CameraThread
+from utils.functions import calcular_distancia_grupo_arucos
 
 # Configuración
 CAMERA_IDS = (0, 1)
 
 MOSTRAR_FRAME = True
+
+DIBUJAR_ARUCOS = True
+
+arucos_alto = config.arucos_alto
+arucos_largo = config.arucos_largo
+arucos_ancho = config.arucos_ancho
+
 
 # Detector ArUco
 dictionary = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_4X4_1000)
@@ -26,11 +36,28 @@ camera_1 = CameraThread(CAMERA_IDS[1]).start()
 print("Cámaras inicializadas.")
 print("Esperando frames...")
 
+# Contador de frames para detectar arucos y medir
+frame_count = 0
+
+# Inicializar listas para hayar la moda de mediciones
+list_ancho = []
+list_largo = []
+list_alto = []
+
+# Estados
+medicion_realizada = False
+mostrar_primer_mensaje = True
 
 try:
     while True:
+        frame_count += 1
         frame_0 = camera_0.read()
         frame_1 = camera_1.read()
+
+        # Si todo esta correcto notificar que puede empezar a medir
+        if mostrar_primer_mensaje:
+            print("...Esperando un objeto para medir...")
+            mostrar_primer_mensaje = False
 
         # Esperar hasta tener ambas imágenes
         if frame_0 is None or frame_1 is None:
@@ -40,16 +67,103 @@ try:
         # Unir imágenes
         imagen_unida = cv2.hconcat([frame_0, frame_1])
 
-        # Detectar sobre escala de grises
-        gray = cv2.cvtColor(imagen_unida, cv2.COLOR_BGR2GRAY)
+        # Detectar ArUcos cada x frames
+        if frame_count % 10 == 0:
+            # Detectar sobre escala de grises
+            gray = cv2.cvtColor(imagen_unida, cv2.COLOR_BGR2GRAY)
 
-        corners, ids, rejects = detector.detectMarkers(gray)
+            corners, ids, rejects = detector.detectMarkers(gray)
 
-        # Dibujar detecciones
-        if ids is not None:
-            cv2.aruco.drawDetectedMarkers(imagen_unida, corners, ids)
+            # Dibujar detecciones
+            if ids is not None and DIBUJAR_ARUCOS:
+                cv2.aruco.drawDetectedMarkers(imagen_unida, corners, ids)
 
-        # Mostrar
+            # Tomar medidas largo, alto, ancho, volumen
+            if ids is not None:
+                ids_detectados = ids.flatten()
+                ancho = calcular_distancia_grupo_arucos(
+                    [
+                        arucos_ancho[0].medir(ids_detectados),
+                        arucos_ancho[1].medir(ids_detectados),
+                        arucos_ancho[2].medir(ids_detectados),
+                        arucos_ancho[3].medir(ids_detectados),
+                        arucos_ancho[4].medir(ids_detectados),
+                    ]
+                )
+                largo = calcular_distancia_grupo_arucos(
+                    [
+                        arucos_largo[0].medir(ids_detectados),
+                        arucos_largo[1].medir(ids_detectados),
+                        arucos_largo[2].medir(ids_detectados),
+                        arucos_largo[3].medir(ids_detectados),
+                        arucos_largo[4].medir(ids_detectados),
+                    ]
+                )
+                alto = calcular_distancia_grupo_arucos(
+                    [
+                        arucos_alto[0].medir(ids_detectados),
+                        arucos_alto[1].medir(ids_detectados),
+                        arucos_alto[2].medir(ids_detectados),
+                        arucos_alto[3].medir(ids_detectados),
+                        arucos_alto[4].medir(ids_detectados),
+                    ]
+                )
+
+
+                # Registrar medidas en lista si volumen > 50000
+                if (ancho > 25 and largo > 25 and alto > 25) and not medicion_realizada:
+                    list_ancho.append(ancho)
+                    list_ancho = list_ancho[-20:]
+                    list_largo.append(largo)
+                    list_largo = list_largo[-20:]
+                    list_alto.append(alto)
+                    list_alto = list_alto[-20:]
+
+                    # Mostrar medidas validas que serán registradas
+                    print("----------")
+                    print(f"ancho: {ancho} mm")
+                    print(f"largo: {largo} mm")
+                    print(f"alto: {alto} mm")
+                    print("**********")
+                    print(f"ancho: {ancho/10:.2f} cm")
+                    print(f"largo: {largo/10:.2f} cm")
+                    print(f"alto: {alto/10:.2f} cm")
+                    print("----------")
+
+                    # Guardar medidas si ya hay 10 registros
+                    if len(list_ancho) == 20 and len(list_largo) == 20 and len(list_alto) == 20:
+                        moda_ancho = mode(list_ancho)
+                        moda_largo = mode(list_largo)
+                        moda_alto = mode(list_alto)
+                        volumen = moda_ancho * moda_largo * moda_alto
+                        print("\nMedidas registradas\n")
+                        print("----------")
+                        print(f"ancho: {moda_ancho} mm")
+                        print(f"largo: {moda_largo} mm")
+                        print(f"alto: {moda_alto} mm")
+                        print(f"volumen: {volumen} mm3")
+                        print("**********")
+                        print(f"ancho: {moda_ancho/10:.2f} cm")
+                        print(f"largo: {moda_largo/10:.2f} cm")
+                        print(f"alto: {moda_alto/10:.2f} cm")
+                        print(f"volumen: {volumen/1000} cm3")
+                        print("----------")
+
+                        print("<<<<<<<Ya puede retirar el objeto>>>>>>>\n")
+                        medicion_realizada = True
+
+                elif (ancho <= 25 and largo <= 175 and alto <= 25) and medicion_realizada:
+                    if medicion_realizada:
+                        print("...Esperando un objeto para medir...")
+
+                    list_ancho.clear()
+                    list_largo.clear()
+                    list_alto.clear()
+
+                    medicion_realizada = False
+
+
+        # Mostrar imagen de camaras
         if MOSTRAR_FRAME:
             cv2.imshow("Imagen_unida", imagen_unida)
 
